d5233f3e9f4aaba1a5dcd8d7ce78446b08a8874d
HSM <iobincn@gmail.com>
2020-10-05 21:00:58 +0800

    update

--- CTFDecode.py
+++ CTFDecode.py
@@ -5,12 +5,12 @@ from Base.scanBaseType import ScanBaseType
 from Base.baseDecode import BaseDecode
 def printHelp():
     print(" Power For HSM")
-    print("NAME: \n   CTFDecode\t A powerful decoding tool\tFrom jjusec HSM\n\n")
+    print("NAME: \n   CTFDecode\t A powerful decoding tool\tPower By HSM\n\n")
     print("DESCRIPTION:\t由JJUSEC团队HSM编写的用于CTF比赛的解码工具\n")
     print("\nOPTIONS:\n")
-    print("\t-b    --baseCheck    base Type Check")
-    print("\t--basedecode base家族解密")
-    print("\t-h    --help    help")
+    print("\t-b\t--baseCheck    查询可能使用过的Base编码类型")
+    print("\t--basedecode\tbase家族解密")
+    print("\t-h\t--help\thelp")
     # print("------")
 
 def main(argv):
